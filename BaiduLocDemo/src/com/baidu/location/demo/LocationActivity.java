bcd38f6d1b77ac0f3e9324686a980875f5d05cf0
Your Name <you@example.com>
2016-07-04 18:59:44 +0800

    换界面

--- BaiduLocDemo/src/com/baidu/location/demo/LocationActivity.java
+++ BaiduLocDemo/src/com/baidu/location/demo/LocationActivity.java
@@ -25,11 +25,16 @@ import com.baidu.location.service.NetworkTask;
 import com.baidu.location.service.Utility;
 import com.baidu.location.service.infoData;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -52,6 +57,10 @@ import android.widget.TextView;
  *
  */
 public class LocationActivity extends Activity {
+	private final int SDK_PERMISSION_REQUEST = 127;
+	private String permissionInfo;
+	
+	
 	private LocationService locationService;
 	private TextView LocationResult;
 	private Button startLocation;
@@ -69,6 +78,7 @@ public class LocationActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		// -----------demo view config ------------
 		setContentView(R.layout.location);
+		getPersimmions();
 		LocationResult = (TextView) findViewById(R.id.textView1);
 		LocationResult.setMovementMethod(ScrollingMovementMethod.getInstance());
 		startLocation = (Button) findViewById(R.id.addfence);
@@ -110,7 +120,19 @@ public class LocationActivity extends Activity {
 
 	}; 
 
-		
+	//检测是否wifi在线
+	public boolean checkNetworkConnection(Context context)  
+	   {  
+	       final ConnectivityManager connMgr = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);  
+	  
+	       final android.net.NetworkInfo wifi =connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);  
+	       	  
+	       if(wifi.isAvailable())  
+	           return true;  
+	       else  
+	           return false;  
+	   }  	
+	
 	//传送数据
 	protected void postData(String GPSURL) {
 		// TODO Auto-generated method stub
@@ -123,7 +145,8 @@ public class LocationActivity extends Activity {
 		HashMap<String, String> localHashMap = new HashMap<String, String>();
 		
 		localHashMap.put(Utility.toUtf8("gpsdata"),jsonArray.toString());
-		
+		//检测是否wifi在线，是就传数据 否则取消
+		if(checkNetworkConnection(this)){
 		taskPool.addHttpPostTask(GPSURL, localHashMap,new AbsHttpTask() {
 			
 			@Override
@@ -153,7 +176,8 @@ public class LocationActivity extends Activity {
 				}
 			}
 			
-		});	
+		});
+		}
 	}
 	
 	//String  to json
@@ -390,4 +414,59 @@ public class LocationActivity extends Activity {
 		}
 
 	};
+	
+	@TargetApi(23)
+	private void getPersimmions() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			ArrayList<String> permissions = new ArrayList<String>();
+			/***
+			 * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+			 */
+			// 定位精确位置
+			if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+				permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+			}
+			if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+				permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+			}
+			/*
+			 * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+			 */
+			// 读写权限
+			if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+				permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+			}
+			// 读取电话状态权限
+			if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+				permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
+			}
+			
+			if (permissions.size() > 0) {
+				requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+			}
+		}
+	}
+
+	@TargetApi(23)
+	private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+		if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请	
+			if (shouldShowRequestPermissionRationale(permission)){
+				return true;
+			}else{
+				permissionsList.add(permission);
+				return false;
+			}
+				
+		}else{
+			return true;
+		}
+	}
+
+	@TargetApi(23)
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		// TODO Auto-generated method stub
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		
+	}
 }
